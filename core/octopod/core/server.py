9912904823bf13b30e1829027719108cbc19221b
Kevin Zhang <me@kevz.dev>
2024-09-11 21:38:55 -0700

    Add BTC price.

--- core/octopod/core/server.py
+++ core/octopod/core/server.py
@@ -1,3 +1,7 @@
+import json
+import requests
+
+from redis import Redis
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
@@ -5,6 +9,8 @@ from octopod.config import config
 from octopod.core.content.router import router as content_router
 from octopod.core.creator.router import router as creator_router
 from octopod.core.user.router import router as user_router
+
+redis = Redis(host=config.REDIS_HOST)
 
 app = FastAPI(
     title="Octopod",
@@ -21,3 +27,19 @@ app.add_middleware(
 app.include_router(content_router)
 app.include_router(creator_router)
 app.include_router(user_router)
+
+@app.get("/btc_price")
+def btc_price():
+    if btc_price := redis.get("btc_price"):
+        return json.loads(btc_price)
+
+    data = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json").json()
+    result = {
+        "USD": data["bpi"]["USD"]["rate_float"],
+    }
+    redis.set(
+        "btc_price",
+        json.dumps(result),
+        ex=60,
+    )
+    return result
